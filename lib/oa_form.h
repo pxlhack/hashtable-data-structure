310bd6624b1e937ed7972de1b7aaa552eb80b58e
pxlhack <nellsouhd@gmail.com>
2022-12-25 17:23:25 +0700

    fixed oa_form.h

--- lib/oa_form.h
+++ lib/oa_form.h
@@ -1,7 +1,7 @@
 #ifndef HASHTABLE2_OA_FORM_H
 #define HASHTABLE2_OA_FORM_H
 
-#include <vector>
+#include <sstream>
 #include "oa_node.h"
 #include "table_form.h"
 
@@ -24,10 +24,120 @@ public:
         }
     }
 
+    bool insert(string k, T data) override {
+        int i = 0;
+        int pos = -1;
+        int j = hashFunction(k, i);
+        this->nodesCounter = 0;
 
+
+        while (i != this->capacity && !isFree(j)) {
+            this->nodesCounter++;
+
+            j = hashFunction(k, i);
+
+            if (pos == -1 && isDeleted(j)) {
+                pos = j;
+            }
+
+            if (isBusy(j) && this->arr[j]->getKey() == k) {
+                return false;
+            }
+
+            i++;
+        }
+
+        if (i == this->capacity && pos == -1) {
+            return false;
+        }
+
+        if (pos == -1) {
+            pos = j;
+        }
+
+        this->nodesCounter++;
+        this->arr[pos]->setKey(k);
+        this->arr[pos]->setValue(data);
+        this->arr[pos]->setBusy();
+        this->size++;
+        return true;
+    }
+
+    bool remove(string k) override {
+        unsigned index = getIndex(k);
+        if (index != -1) {
+            this->arr[index]->setDeleted();
+            this->size--;
+            return true;
+        }
+
+        return false;
+    }
+
+    T get(string k) override {
+        unsigned index = getIndex(k);
+        if (index != -1) {
+            return this->arr[index]->getValue();
+        }
+
+        throw "not found!";
+    }
+
+    void clear() override {
+        for (OANode<T> *node: nodes) {
+            if (node->isBusy()) {
+                node->setDeleted();
+            }
+        }
+    }
+
+    string toString() override {
+        stringstream sstr;
+        for (int i = 0; i < this->capacity; ++i) {
+            string stringView = "[";
+            stringView += std::to_string(i);
+            stringView += "] ";
+            stringView += this->arr[i]->toString();
+            sstr << stringView << endl;
+        }
+        return sstr.str();
+    }
 
 private:
     OANode<T> **nodes;
+
+    int hashFunction(string str, int i) {
+        return (this->hornerFunction(str) + i) % this->capacity;
+    }
+
+    bool isDeleted(int j) {
+        return this->nodes[j]->isDeleted();
+    }
+
+    bool isBusy(int j) {
+        return this->nodes[j]->isBusy();
+    }
+
+    bool isFree(int j) {
+        return this->nodes[j]->isFree();
+    }
+
+    unsigned getIndex(string k) {
+        int i = 0;
+        int j = -1;
+        this->nodesCounter = 0;
+        do {
+            this->nodesCounter++;
+            j = hashFunction(k, i);
+            if (isBusy(j) && this->arr[j]->getKey() == k) {
+                break;
+            }
+            i++;
+        } while (i != this->capacity || !isFree(j));
+
+        return j;
+    }
+
 };
 
 
--- lib/table_form.h
+++ lib/table_form.h
@@ -2,6 +2,7 @@
 #define HASHTABLE2_TABLE_FORM_H
 
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -21,11 +22,11 @@ public:
 
     virtual bool remove(string k) = 0;
 
-    virtual T get(string K) = 0;
+    virtual T get(string k) = 0;
 
     virtual void clear() = 0;
 
-    virtual void toString() = 0;
+    virtual string toString() = 0;
 
     bool isEmpty() {
         return this->size == 0;
@@ -43,6 +44,18 @@ public:
         return this->nodesCounter;
     }
 
+    int hornerFunction(string str) {
+        unsigned k = 0;
+        for (unsigned i = 0; i < str.size(); ++i) {
+            k += pow(32, str.size() - 1 - i) * (str.at(i) - 'A' + 1);
+        }
+
+        double g_r = 0.6180339887;
+
+        double X = g_r * k;
+        X = X - (unsigned) X;
+        return (unsigned) (X * this->capacity);
+    }
 
 protected:
     unsigned size;
