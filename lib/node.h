68a76b2291a8ee75eae659def16bf602c81ee9ca
pxlhack <nellsouhd@gmail.com>
2022-12-25 15:56:59 +0700

    add oa_node.h

--- lib/node.h
+++ lib/node.h
@@ -24,6 +24,9 @@ public:
         this->data = data;
     }
 
+    virtual string toString() = 0;
+
+
 protected:
     string key;
     T data;
---
+++ lib/oa_node.h
@@ -0,0 +1,53 @@
+#ifndef HASHTABLE2_OA_NODE_H
+#define HASHTABLE2_OA_NODE_H
+
+#include "node.h"
+
+enum states {
+    FREE,
+    BUSY,
+    DELETED
+};
+
+template<typename T>
+class OANode : public Node<T> {
+
+    OANode() {
+        state = states::FREE;
+    }
+
+    void setDeleted() {
+        this->state = states::DELETED;
+    }
+
+    void setBusy() {
+        this->state = states::BUSY;
+    }
+
+    bool isFree() {
+        return state == states::FREE;
+    }
+
+    bool isBusy() {
+        return state == states::BUSY;
+    }
+
+    bool isDeleted() {
+        return state == states::DELETED;
+    }
+
+    string toString() override {
+        string states[] = {"FREE", "BUSY", "DELETED"};
+        string stringView = "(" + states[state] + ") ";
+        if (isBusy()) {
+            stringView += "Key: " + this->key + " " + "Data: " + to_string(this->value);
+        }
+        return stringView;
+    }
+
+private:
+    states state;
+};
+
+
+#endif //HASHTABLE2_OA_NODE_H
