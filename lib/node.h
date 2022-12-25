a6514833682ea5e82eea3e425c4161bf6e1f94fa
pxlhack <nellsouhd@gmail.com>
2022-12-25 15:51:48 +0700

    add node.h

---
+++ lib/node.h
@@ -0,0 +1,33 @@
+#ifndef HASHTABLE2_NODE_H
+#define HASHTABLE2_NODE_H
+
+#include <string>
+
+using namespace std;
+
+template<typename T>
+class Node {
+public:
+    string getKey() const {
+        return key;
+    }
+
+    void setKey(string key) {
+        this->key = key;
+    }
+
+    T getData() const {
+        return data;
+    }
+
+    void setData(T data) {
+        this->data = data;
+    }
+
+protected:
+    string key;
+    T data;
+};
+
+
+#endif //HASHTABLE2_NODE_H
